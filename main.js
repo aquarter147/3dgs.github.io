bddac0ec2421645222faedc32fe5c6c69f3e6319
nnthao109 <nhuthaoksa@gmail.com>
2025-06-02 03:21:17 +0000

    Change the scene

--- main.js
+++ main.js
@@ -746,8 +746,10 @@ async function main() {
     const url = new URL(
         // "nike.splat",
         // location.href,
-        params.get("url") || "train.splat",
-        "https://huggingface.co/cakewalk/splat-data/resolve/main/",
+        // params.get("url") || "train.splat",
+        // "https://huggingface.co/cakewalk/splat-data/resolve/main/",
+        params.get("url") || "tea_time.splat",
+        location.href
     );
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
